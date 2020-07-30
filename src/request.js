764d5d1620a1051d2e9713725dd087fb2cd67231
MinCheTsai <minche@softchef.com>
2020-07-30 10:42:17 +0800

    Add Request.validate support options override

--- src/request.js
+++ src/request.js
@@ -4,160 +4,166 @@ const Busboy = require('busboy')
 const Joi = require('./validator')
 
 class Request {
-    constructor(event) {
-        this.event = event || {}
-        this.headers = this.event.headers || {}
-        this.isMultipartFormData = /^multipart\/form-data.*$/.test(this.headers['Content-Type'] || this.headers['content-type'])
-        this.parameters = this.event.pathParameters || {}
-        this.query = this.event.queryStringParameters || {}
-        this.body = {}
-        this.files = {}
-        try {
-            if (this.isMultipartFormData) {
-                this.busboy = new Busboy({
-                headers: {
-                    'content-type': this.headers['Content-Type'] || this.headers['content-type']
-                }
-                })
-            } else if (typeof this.event.body === 'string') {
-                this.body = JSON.parse(this.event.body) || {}
-            } else if (typeof this.event.body === 'object') {
-                this.body = this.event.body || {}
-            } else {
-                this.body = {}
-            }
-        } catch (error) {
-            this.body = {}
+  constructor(event) {
+    this.event = event || {}
+    this.headers = this.event.headers || {}
+    this.isMultipartFormData = /^multipart\/form-data.*$/.test(this.headers['Content-Type'] || this.headers['content-type'])
+    this.parameters = this.event.pathParameters || {}
+    this.query = this.event.queryStringParameters || {}
+    this.body = {}
+    this.files = {}
+    try {
+      if (this.isMultipartFormData) {
+        this.busboy = new Busboy({
+        headers: {
+          'content-type': this.headers['Content-Type'] || this.headers['content-type']
         }
-    }
-    parameter(key) {
-        let parameter = this.parameters[key] || null
-        if (parameter) {
-            parameter = decodeURI(parameter)
-        }
-        return parameter
-    }
-    get(key, defaultValue = null) {
-        let result = this.query[key]
-        if (result === undefined) {
-            result = defaultValue
-        }
-        return result
-    }
-    input(key, defaultValue = null) {
-        let result = this.body[key]
-        if (result === undefined) {
-            result = this.query[key] || defaultValue
-        }
-        return result
-    }
-    inputs(keys) {
-        const inputs = {}
-        for (let key of keys) {
-            let input = this.input(key)
-            if (input === undefined) {
-                input = this.get(key)
-            }
-            inputs[key] = input
-        }
-        return inputs
-    }
-    has(key) {
-        if (this.query[key] !== undefined) {
-            return true
-        } else if (this.body[key] !== undefined) {
-            return true
-        }
-        return false
-    }
-    async file(key) {
-        if (!this.busboy._finished) {
-            await this._processFiles()
-        }
-        return Promise.resolve(this.files[key].binary || null)
-    }
-    headers() {
-        return this.event.headers
-    }
-    header(key) {
-        return this.event.headers[key] || null
-    }
-    user(attribute) {
-        const requestContext = this.event.requestContext || {}
-        const authorizer = requestContext.authorizer || {}
-        const claims = authorizer.claims || {}
-        const identity = authorizer.identity || 'default'
-        let user
-        if (typeof claims === 'string') {
-            user = JSON.parse(claims)
-        } else {
-            user = claims
-        }
-        user.identity = identity
-        user.username = user['cognito:username'] || user.sub
-        if (attribute) {
-            return user[attribute] || null
-        } else {
-            return user
-        }
-    }
-    validate(keysProvider) {
-        const keys = keysProvider(Joi)
-        const schema = Joi.object().keys(keys)
-        const result = schema.validate(
-            this.inputs(
-                Object.keys(keys)
-            )
-        )
-        if (result.error) {
-            let messages = {}
-            for (let error of result.error.details) {
-                messages[error.context.key] = error.message
-            }
-            return {
-                error: true,
-                messages
-            }
-        }
-        return {
-            error: false
-        }
-    }
-    _processFiles() {
-        return new Promise((resolve, reject) => {
-          try {
-            this.busboy.on('field', (fieldName, value, fieldNameTruncated, valTruncated) => {
-              this.body[fieldName] = value
-            })
-            this.busboy.on('file', (fieldName, file, filename, encoding, mimeType) => {
-              if (!this.files[fieldName]) {
-                this.files[fieldName] = {
-                  binary: Buffer.from(''),
-                  filename,
-                  encoding,
-                  mimeType
-                }
-              }
-              file.on('data', (data) => {
-                this.files[fieldName].binary += data
-              })
-              file.on('end', () => {})
-            })
-            this.busboy.on('error', error => {
-              return reject(
-                new Error(`Parse error: ${error}`)
-              )
-            })
-            this.busboy.on('finish', () => {
-              resolve(true)
-            })
-            this.busboy.write(this.event.body, this.event.isBase64Encoded ? 'base64' : 'binary')
-            this.busboy.end()
-          } catch (error) {
-            reject(error)
-          }
         })
+      } else if (typeof this.event.body === 'string') {
+        this.body = JSON.parse(this.event.body) || {}
+      } else if (typeof this.event.body === 'object') {
+        this.body = this.event.body || {}
+      } else {
+        this.body = {}
       }
+    } catch (error) {
+      this.body = {}
+    }
+  }
+  parameter(key) {
+    let parameter = this.parameters[key] || null
+    if (parameter) {
+      parameter = decodeURI(parameter)
+    }
+    return parameter
+  }
+  get(key, defaultValue = null) {
+    let result = this.query[key]
+    if (result === undefined) {
+      result = defaultValue
+    }
+    return result
+  }
+  input(key, defaultValue = null) {
+    let result = this.body[key]
+    if (result === undefined) {
+      result = this.query[key] || defaultValue
+    }
+    return result
+  }
+  inputs(keys) {
+    const inputs = {}
+    for (let key of keys) {
+      let input = this.input(key)
+      if (input === undefined) {
+        input = this.get(key)
+      }
+      inputs[key] = input
+    }
+    return inputs
+  }
+  has(key) {
+    if (this.query[key] !== undefined) {
+      return true
+    } else if (this.body[key] !== undefined) {
+      return true
+    }
+    return false
+  }
+  async file(key) {
+    if (!this.busboy._finished) {
+      await this._processFiles()
+    }
+    return Promise.resolve(this.files[key].binary || null)
+  }
+  headers() {
+    return this.event.headers
+  }
+  header(key) {
+    return this.event.headers[key] || null
+  }
+  user(attribute) {
+    const requestContext = this.event.requestContext || {}
+    const authorizer = requestContext.authorizer || {}
+    const claims = authorizer.claims || {}
+    const identity = authorizer.identity || 'default'
+    let user
+    if (typeof claims === 'string') {
+      user = JSON.parse(claims)
+    } else {
+      user = claims
+    }
+    user.identity = identity
+    user.username = user['cognito:username'] || user.sub
+    if (attribute) {
+      return user[attribute] || null
+    } else {
+      return user
+    }
+  }
+
+  validate(keysProvider, options = {}) {
+    const keys = keysProvider(Joi)
+    const schema = Joi.object().keys(keys)
+    const result = schema.validate(
+      this.inputs(
+        Object.keys(keys)
+      ),
+      {
+        abortEarly: false,
+        ...options
+      }
+    )
+    if (result.error) {
+      let messages = {}
+      for (let error of result.error.details) {
+        messages[error.context.key] = error.message
+      }
+      return {
+        error: true,
+        messages
+      }
+    }
+    return {
+      error: false
+    }
+  }
+
+  _processFiles() {
+    return new Promise((resolve, reject) => {
+      try {
+        this.busboy.on('field', (fieldName, value, fieldNameTruncated, valTruncated) => {
+          this.body[fieldName] = value
+        })
+        this.busboy.on('file', (fieldName, file, filename, encoding, mimeType) => {
+          if (!this.files[fieldName]) {
+            this.files[fieldName] = {
+              binary: Buffer.from(''),
+              filename,
+              encoding,
+              mimeType
+            }
+          }
+          file.on('data', (data) => {
+            this.files[fieldName].binary += data
+          })
+          file.on('end', () => {})
+        })
+        this.busboy.on('error', error => {
+          return reject(
+            new Error(`Parse error: ${error}`)
+          )
+        })
+        this.busboy.on('finish', () => {
+          resolve(true)
+        })
+        this.busboy.write(this.event.body, this.event.isBase64Encoded ? 'base64' : 'binary')
+        this.busboy.end()
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
 }
 
 module.exports = Request
